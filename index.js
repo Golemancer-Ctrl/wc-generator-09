08f5aa765c22f1eb0e4795a732c93136fe1cd783
Sheev <ericmarcypro@gmail.com>
2022-10-16 14:24:20 -0400

    imported inquirer, fs, generateMarkdown.  added object array of questions.

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation?'
+    },
+
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve?'
+    },
+
+    {
+        type: 'input',
+        name: 'challenge',
+        message: 'Describe the challenges you faced.'
+    },
+
+    {
+        type: 'input',
+        name: 'future',
+        message: 'Add some possibilities for future development.'
+    },
+
+    {
+        type: 'input',
+        name: 'path',
+        message: 'Provide a relative path to a screenshot or demo.'
+    },
+
+    {
+        type: 'input',
+        name: 'url',
+        message: 'Provide a url to the deployed site, otherwise N/A.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
